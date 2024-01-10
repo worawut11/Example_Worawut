8034fe49d2acd6ccc5b45d9ca583761a3e3a5cf1
worawut11 <136050099+worawut11@users.noreply.github.com>
2024-01-10 09:41:27 +0700

    Add files via upload

---
+++ responsive_registration_form___codinglab.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3310
+-- Generation Time: Jan 10, 2024 at 03:39 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `responsive registration form | codinglab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `connect`
+--
+
+CREATE TABLE `connect` (
+  `Full Name` varchar(20) NOT NULL,
+  `Username` varchar(20) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Phone Number` varchar(10) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Confirm Password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
